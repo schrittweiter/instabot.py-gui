5dc2aa894ab61b8481df6e971b468fdeb12584a9
David <dk@schrittweiter.de>
2018-04-30 17:02:52 +0200

    updatet: tag search

--- application/js/app.js
+++ application/js/app.js
@@ -27,10 +27,10 @@ app.controller('appController', function appController($scope) {
         'likes_tag':        parseInt(localStorage.getItem('likes_tag')),
         'follows_day':      parseInt(localStorage.getItem('follows_day')),
         'unfollows_day':    parseInt(localStorage.getItem('unfollows_day')),
-        'hashtags_white':   localStorage.getItem('hashtags_white'),
-        'hashtags_black':   localStorage.getItem('hashtags_black'),
-        'user_black':       localStorage.getItem('user_black'),
-        'user_white':       localStorage.getItem('user_white')
+        'hashtags_white':   (localStorage.getItem('hashtags_white') ? localStorage.getItem('hashtags_white').split(',') : null),
+        'hashtags_black':   (localStorage.getItem('hashtags_black') ? localStorage.getItem('hashtags_black').split(',') : null),
+        'user_black':       (localStorage.getItem('user_black') ? localStorage.getItem('user_black').split(',') : null),
+        'user_white':       (localStorage.getItem('user_white') ? localStorage.getItem('user_white').split(',') : null)
     }
 
     // # reset log
@@ -43,16 +43,28 @@ app.controller('appController', function appController($scope) {
 
         // # store inputs in local storage
 
-        localStorage.setItem('username', bot.username);
-        localStorage.setItem('password', bot.password);
-        localStorage.setItem('likes', bot.likes);
-        localStorage.setItem('likes_tag', bot.likes_tag);
-        localStorage.setItem('follows_day', bot.follows_day);
-        localStorage.setItem('unfollows_day', bot.unfollows_day);
-        localStorage.setItem('hashtags_white', bot.hashtags_white);
-        localStorage.setItem('hashtags_black', bot.hashtags_black);
-        localStorage.setItem('user_black', bot.user_black);
-        localStorage.setItem('user_white', bot.user_white);
+        localStorage.setItem('username',        bot.username);
+        localStorage.setItem('password',        bot.password);
+        localStorage.setItem('likes',           bot.likes);
+        localStorage.setItem('likes_tag',       bot.likes_tag);
+        localStorage.setItem('follows_day',     bot.follows_day);
+        localStorage.setItem('unfollows_day',   bot.unfollows_day);
+        
+        if(bot.hashtags_white) {
+            localStorage.setItem('hashtags_white',  (bot.hashtags_white ? Array.prototype.map.call(bot.hashtags_white, s => s.text).toString()  : null));
+        }
+
+        if(bot.hashtags_black) {
+            localStorage.setItem('hashtags_black',  (bot.hashtags_black ? Array.prototype.map.call(bot.hashtags_black, s => s.text).toString()  : null));
+        }
+
+        if(bot.user_black) {
+            localStorage.setItem('user_black',      (bot.user_black ? Array.prototype.map.call(bot.user_black, s => s.text).toString()  : null));
+        }
+        
+        if(bot.user_white) {
+            localStorage.setItem('user_white',      (bot.user_white ? Array.prototype.map.call(bot.user_white, s => s.text).toString()  : null));
+        }
 
         // # set states
        
@@ -153,14 +165,14 @@ app.controller('appController', function appController($scope) {
             }
 
 
-            tags_compare = temp_tags;
+            tags_compare = Array.prototype.map.call(temp_tags, s => s.text).toString();
             console.log(tags_compare);
 
             if(data.results.length > 0) {
                 $(resultWrapper).html('');
                 $(resultWrapper).html('<div class="uk-alert-primary" uk-alert><a class="uk-alert-close" uk-close></a><p>Add hashtags to your whitelist by clicking them.</p></div>');
                 $.each(data.results, function(i, item) {
-                    if( $.inArray(item.tag, tags_compare) != -1){
+                    if( tags_compare.split(',').indexOf(item.tag) > -1 ) {
                     } else {
                         $(resultWrapper).append('<span class="uk-label uk-label-primary hashtag" data-value="'+item.tag+'">#'+item.tag+'</span>');
                     }
